0edfd8b5812342b34d0ca8cbb1b946685975f870
Kawwolski <47087721+Kawwolski@users.noreply.github.com>
2019-06-14 15:32:45 +0200

    Add files via upload

---
+++ Zad5/Zad5/BankAccount.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zad5
+{
+   public class BankAccount
+    {
+        public int ind { get; internal set; }
+        public double acount { get; internal set; }
+
+        private static double offset=0.03;
+
+        public BankAccount(double account,int id)
+        {
+            acount=account;
+            ind=id;
+        }
+
+
+        internal static void Changeoffset(double newOffset)
+        {
+            offset = newOffset;
+            
+        }
+
+        public void GetCash(double cash)
+        {
+            acount = acount - cash - (cash * offset);
+           
+        }
+
+        public void Transfer(BankAccount account2,double cash)
+        {
+            acount = acount - cash - (cash * (0.5*offset));
+            account2.acount = account2.acount + cash;
+        }
+
+    }
+}
---
+++ Zad5/Zad5/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zad5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<BankAccount> list = new List<BankAccount>();
+
+            list.Add(new BankAccount(100.00, 1));
+            list.Add(new BankAccount(150.00, 2));
+            list.Add(new BankAccount(200.00, 3));
+
+            string str;
+                
+                do {
+                Console.WriteLine("1.Stan konta");
+                Console.WriteLine("2.Wypłata pieniedzy z bankomatu");
+                Console.WriteLine("3.Przelew");
+                Console.WriteLine("4.Wyjscie");
+                str = Console.ReadLine();
+                switch (str)
+                {
+                    case "1":
+
+                        Console.WriteLine("Podaj Identyfikator konta");
+                        int id = int.Parse(Console.ReadLine());
+
+                        foreach (var s in list.Where(x => x.ind == id))
+                        {
+                            Console.WriteLine("Identyfikator konta: " + id + ".Stan konta: " + s.acount.ToString());
+
+                        }
+
+                        continue;
+                    case "2":
+
+                        Console.WriteLine("Podaj Identyfikator konta");
+                        id = int.Parse(Console.ReadLine());
+
+                        Console.WriteLine("Podaj kwote ktora chcesz wybrac");
+                        double cash = double.Parse(Console.ReadLine());
+
+                        foreach (var s in list.Where(x => x.ind == id))
+                        {
+                            s.GetCash(cash);
+                        }
+
+                        continue;
+
+                    case "3":
+
+                        Console.WriteLine("Podaj Identyfikator swojego konta");
+                        id = int.Parse(Console.ReadLine());
+
+                        Console.WriteLine("Podaj kwote ktora chcesz przelac");
+                        cash = double.Parse(Console.ReadLine());
+
+                        Console.WriteLine("Podaj Identyfikator drugiego konta");
+                        int fid = int.Parse(Console.ReadLine());
+
+                        foreach (var s in list.Where(x => x.ind == id))
+                        {
+                           BankAccount first = s;
+                            foreach (var t in list.Where(x => x.ind == fid))
+                            {
+                               BankAccount second = t;
+                                first.Transfer(second, cash);
+                            }
+                            
+                        }
+
+                        continue;
+                }
+        } while (str != "Wyjscie");
+
+            Console.ReadKey();
+        }
+    }
+}
+
+
